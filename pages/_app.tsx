dcb0cf7698235b23d6cc27b6096fd1f8a2917f31
Demmy Honore de Vries <demmyhonore@gmail.com>
2022-10-09 17:26:40 +0200

    Add initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const customTheme = {
+  fonts: {
+    heading: `'Noto Sans', sans-serif`,
+    body: `'Noto Sans', sans-serif`,
+  },
+  colors: {
+    brand: {
+      olive: "#b8e0d2",
+      orange: "#ffd6a5",
+      purple: "#bdb2ff",
+      // blue: "#9bf6ff",
+      // brokenWhite: "#fffffc",
+      // darkBlue: "#a0c4ff",
+      // darkGrey: "#666666",
+      // green: "#caffbf",
+      // red: "#ffadad",
+      // violet: "#ffc6ff",
+      // yellow: "#fdffb6",
+    },
+  },
+};
+
+const theme = extendTheme({ ...customTheme });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
